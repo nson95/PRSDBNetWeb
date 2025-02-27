0d4e402f8b3b3e455286954efcf269874dfc6eee
nson95 <nicksonnenberg95@gmail.com>
2025-02-27 17:47:49 -0500

    added controller for user, request and lineitem

---
+++ Controllers/LineItemsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PRSNetWeb.Models;
+
+namespace PRSNetWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineItemsController : ControllerBase
+    {
+        private readonly PrsdbContext _context;
+
+        public LineItemsController(PrsdbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LineItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LineItem>>> GetLineItems()
+        {
+            return await _context.LineItems.ToListAsync();
+        }
+
+        // GET: api/LineItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LineItem>> GetLineItem(int id)
+        {
+            var lineItem = await _context.LineItems.FindAsync(id);
+
+            if (lineItem == null)
+            {
+                return NotFound();
+            }
+
+            return lineItem;
+        }
+
+        // PUT: api/LineItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLineItem(int id, LineItem lineItem)
+        {
+            if (id != lineItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(lineItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LineItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/LineItems
+        [HttpPost]
+        public async Task<ActionResult<LineItem>> PostLineItem(LineItem lineItem)
+        {
+            _context.LineItems.Add(lineItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLineItem", new { id = lineItem.Id }, lineItem);
+        }
+
+        // DELETE: api/LineItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLineItem(int id)
+        {
+            var lineItem = await _context.LineItems.FindAsync(id);
+            if (lineItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.LineItems.Remove(lineItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LineItemExists(int id)
+        {
+            return _context.LineItems.Any(e => e.Id == id);
+        }
+    }
+}
